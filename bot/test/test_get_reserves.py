3b692ff3fd600822ab027c19b137b6e950d7764d
Artur Modesto <modestoartur@gmail.com>
2021-06-26 18:59:19 -0300

    all_tests_passed
    Signed-off-by: Artur Modesto <modestoartur@gmail.com>

---
+++ bot/test/test_get_reserves.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+import json
+import time
+
+from events import *
+
+
+def test_get_reserves():
+    pairs = json.load(open("files/pairs.json"))
+    start = time.time()
+    r = get_reserves(pairs)
+    end = time.time()
+    print(r)
+    print("cost:", end - start)
